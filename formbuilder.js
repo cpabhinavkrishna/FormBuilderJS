fd85890caff909307c5d66d0f3e4c039aded1c3c
cpabhinavkrishna <c.p.abhinavkrrishna@gmail.com>
2025-02-23 09:04:10 +0530

    bug fixes

--- formbuilder.js
+++ formbuilder.js
@@ -1,71 +1,86 @@
 (function () {
-    function buildForm(containerSelector) {
-        const container = document.querySelector(containerSelector);
-        if (!container) {
-            console.error('Container not found');
-            return;
-        }
-
-        // Create form element
-        const form = document.createElement('form');
-        form.setAttribute('id', 'dynamicForm');
-
-        // Create field container
-        const fieldContainer = document.createElement('div');
-        fieldContainer.setAttribute('id', 'fieldContainer');
-        form.appendChild(fieldContainer);
-
-        // Create add field Combobox
-        const chooseFieldLable = document.createElement('input');
-        chooseFieldLable.setAttribute("for","chooseField");
-        chooseFieldLable.setAttribute("id","chooseField");
-        chooseFieldLable.setAttribute("name","fieldType");
-        const fieldTypeDataList = document.createElement("datalist");
-        fieldTypeDataList.setAttribute("id","fieldTypeList");
-        const option1 = document.createElement("option").setAttribute("value","text");
-        const option2 = document.createElement("option").setAttribute("value","number");
-        const option3 = document.createElement("option").setAttribute("value","email");
-        const option4 = document.createElement("option").setAttribute("value","password");
-        const chooseFieldInput = document.createElement("input");
-        fieldTypeDataList.appendChild(option1);
-        fieldTypeDataList.appendChild(option2);
-        fieldTypeDataList.appendChild(option3);
-        fieldTypeDataList.appendChild(option4);
-        chooseFieldInput.setAttribute("list","fieldTypeList");
-        chooseFieldInput.setAttribute("id","chooseField");
-        chooseFieldLable.addEventListener('click', function (e) {
-            addField(fieldContainer);
-        });
-        
-        // Create submit button
-        const submitButton = document.createElement('button');
-        submitButton.textContent = 'Submit';
-        submitButton.type = 'submit';
-        form.appendChild(chooseFieldLable);
-        form.appendChild(submitButton);
-
-        container.appendChild(form);
+  function buildForm(containerSelector) {
+    const container = document.querySelector(containerSelector);
+    if (!container) {
+      console.error("Container not found");
+      return;
     }
 
-    function addField(container) {
-        const fieldWrapper = document.createElement('div');
-        fieldWrapper.classList.add('field-wrapper');
+    // Create form element
+    const form = document.createElement("form");
+    form.setAttribute("id", "dynamicForm");
 
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.placeholder = 'Enter value';
+    // Create field container
+    const fieldContainer = document.createElement("div");
+    fieldContainer.setAttribute("id", "fieldContainer");
+    form.appendChild(fieldContainer);
 
-        const removeButton = document.createElement('button');
-        removeButton.textContent = 'Remove';
-        removeButton.type = 'button';
-        removeButton.addEventListener('click', function () {
-            container.removeChild(fieldWrapper);
-        });
+    // Create add field Combobox
+    const chooseField = document.createElement("select");
+    chooseField.setAttribute("for", "chooseField");
+    chooseField.setAttribute("id", "chooseField");
+    chooseField.setAttribute("name", "fieldType");
+    const option1 = document.createElement("option");
+    option1.setAttribute("value", "text");
+    option1.setAttribute("label", "text");
+    const option2 = document.createElement("option");
+    option2.setAttribute("value", "number");
+    option2.setAttribute("label", "number");
+    const option3 = document.createElement("option");
+    option3.setAttribute("value", "email");
+    option3.setAttribute("label", "email");
+    const option4 = document.createElement("option");
+    option4.setAttribute("value", "password");
+    option4.setAttribute("label", "password");
+    const option = document.createElement("option");
+    option.setAttribute("value", "");
+    option.setAttribute("label", "Select an option");
+    chooseField.appendChild(option);
+    chooseField.appendChild(option1);
+    chooseField.appendChild(option2);
+    chooseField.appendChild(option3);
+    chooseField.appendChild(option4);
+    chooseField.addEventListener("change", function () {
+      addField(fieldContainer);
+    });
 
-        fieldWrapper.appendChild(input);
-        fieldWrapper.appendChild(removeButton);
-        container.appendChild(fieldWrapper);
+    form.appendChild(chooseField);
+
+    container.appendChild(form);
+  }
+
+  function addField(container) {
+    const fieldWrapper = document.createElement("div");
+    fieldWrapper.classList.add("field-wrapper");
+    const value = document.getElementById("chooseField").value;
+    const input = document.createElement("input");
+    if (value === "text") {
+        input.type = "text";
+      input.placeholder = "Enter text";
+    } else if (value === "number") {
+      input.type = "text";
+      input.placeholder = "Enter number";
+    } else if (value === "email") {
+        input.type = "email";
+      input.placeholder = "Enter email";
+    } else if (value === "password") {
+        input.type = "password";
+      input.placeholder = "Enter password";
+    } else {
+      return;
     }
+ 
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", function () {
+      container.removeChild(fieldWrapper);
+    });
 
-    window.buildForm = buildForm;
+    fieldWrapper.appendChild(input);
+    fieldWrapper.appendChild(removeButton);
+    container.appendChild(fieldWrapper);
+  }
+
+  window.buildForm = buildForm;
 })();
